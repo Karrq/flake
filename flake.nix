676985f132f6575966c4f2a8804c3f08021a4068
Francesco Dainese <franci.dainese@gmail.com>
2023-11-13 22:53:40 +0800

    feat: overlay
    feat: nixGLWrap and prismlauncher

--- flake.nix
+++ flake.nix
@@ -4,21 +4,36 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nixgl = {
+      url = "github:guibou/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" ];
+      imports = [ inputs.flake-parts.flakeModules.easyOverlay ];
       perSystem = { self', inputs', pkgs, ... }:
         let
           callPackage = pkgs.lib.callPackageWith (self'.packages);
           java8 = callPackage ./java8.nix;
+          nixGLWrap = (p:
+            pkgs.writeShellScriptBin p.name ''
+              ${inputs'.nixgl.packages.default} ${p.out} "$@"
+            '');
         in {
           packages.java8u232 = java8 {
             inherit pkgs;
             update = "232";
             build = "b09";
             sha256 = "sha256-hh01uzkzhdCX6f53oQut2tfoKTnZvWBsORWLLiXewSY=";
+          };
+
+          packages.prismlauncher = nixGLWrap pkgs.prismlauncher;
+
+          overlayAttrs = {
+            inherit (self'.packages) prismlauncher java8u232;
           };
         };
     };
